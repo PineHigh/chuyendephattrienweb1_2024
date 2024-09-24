309df7c7fa6ba4f2726f49090dc7167681b3c4ed
PineHigh <trungcaothong@gmail.com>
2024-09-24 08:46:30 +0700

    add oop

---
+++ oop/MyInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+interface IA {
+  public function NotDeclare();
+
+}
+
+interface IB{
+   
+
+}
+interface IC{
+   
+
+}
+interface interface1{
+   
+
+}
+interface interface2{
+   
+
+}
+interface interface3{
+   
+
+}
+
+// interface Interface2 {
+
+// }
+
+// interface Interface3 {
+
+// }z
